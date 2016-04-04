fa2a58f39150df6d084b28a1b257ad5d564ece73
michael dada <mail.michaeldada@gmail.com>
2016-04-04 09:41:48 -0700

    add initial files

---
+++ app/components/new-listing/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    newListingForm() {
+      this.set('newListingForm', true)
+    },
+    newListing() {
+      var params = {
+        title: this.get('title'),
+        description: this.get('description'),
+        contact: this.get('contact'),
+        category: this.get('category')
+      };
+      this.sendAction('newListing', params);
+      this.set('title', null);
+      this.set('description', null);
+      this.set('content', null);
+      this.set('newListingForm', false);
+    }
+  }
+});
---
+++ app/models/listing.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+  contact: DS.attr('string'),
+  category: DS.belongsTo('category', {async: true})
+});
